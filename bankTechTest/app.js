9e2ed6f082b381a50ef8198a59b3a7d14d0cacc7
Guillermina Lorenzo Martinez <guillerminalorenzomartinez@MASTUDE5H47Q6L4.localdomain>
2022-10-24 17:29:00 +0100

    Updated app.js

--- bankTechTest/app.js
+++ bankTechTest/app.js
@@ -5,7 +5,6 @@ const acc = new Account()
 acc.deposit(1000)
 acc.deposit(2000)
 acc.withdrawal(500)
-acc.balance()
 
 const bank = new BankStatement(acc.transactions)
 console.log(bank.printStatement())
