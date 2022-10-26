e3e6fdb37cae948ccf0ef6f71d157f054266c62e
Guillermina Lorenzo Martinez <guillerminalorenzomartinez@MASTUDE5H47Q6L4.localdomain>
2022-10-26 10:36:50 +0100

    More refactoring, 3 tests not passing

--- gildedRose/src/gilded_rose.js
+++ gildedRose/src/gilded_rose.js
@@ -16,44 +16,40 @@ class Shop {
       const AgedBrie = item.name == 'Aged Brie'
       const Backstage = item.name == 'Backstage passes to a TAFKAL80ETC concert'
       const Sulfuras = item.name == 'Sulfuras, Hand of Ragnaros'
-      const Conjured = item.name == 'Conjured Mana Cake'
+      const Conjured = item.name.includes("Conjured")
+      const specialItems = !AgedBrie && !Backstage && !Sulfuras
       const qualityBiggerThanZero = item.quality > 0
       const qualityLessThanFifty = item.quality < 50
       const sellInLessThanEleven = item.sellIn < 11
       const sellInLessThanSix = item.sellIn < 6
-
-      if (!AgedBrie && !Backstage) {
-        if (qualityBiggerThanZero && !Sulfuras) {
-          item.quality -= 1
-        }
-      } else if (qualityLessThanFifty) {
-        item.quality += 1;
-        if (Backstage) {
-          if (sellInLessThanEleven) {           
-            item.quality += 1         
-          }
-          if (sellInLessThanSix) {
-            item.quality += 1
-          }
-        }
-      }
+      const sellInLessThanZero = item.sellIn < 0
 
       if (!Sulfuras) {
         item.sellIn -= 1;
       }
-
-      if (item.sellIn < 0) {
-        if (!AgedBrie) {
-          if (!Backstage) {
-            if (qualityBiggerThanZero && !Sulfuras) {
-              item.quality -= 1
-            }
-          } else {
-            item.quality = 0;
+        
+      if (specialItems) {
+        if (qualityBiggerThanZero) {
+            item.quality -= 1
+          if (sellInLessThanZero) {
+            item.quality -= 1
           }
-        } else if (qualityLessThanFifty) {
-          item.quality += 1;
         }
+      } else if (Backstage) {
+        item.quality += 1;
+        if (sellInLessThanEleven) {           
+          item.quality += 1         
+        }
+        if (sellInLessThanSix) {
+          item.quality += 1
+        }
+        if (sellInLessThanZero) {
+          item.quality = 0
+        }
+      } else if (AgedBrie && qualityLessThanFifty) {
+          item.quality += 1;
+      } else if (Conjured) {
+        item.quality -= 2
       }
     }) 
     return this.items
