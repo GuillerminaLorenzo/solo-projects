5cf88a36127ef15c5382c14e6ccb1035c9e558e9
Guillermina Lorenzo Martinez <guillerminalorenzomartinez@MASTUDE5H47Q6L4.localdomain>
2022-10-27 16:26:30 +0100

    Changed account unit tests

--- bankTechTest/tests/account.test.js
+++ bankTechTest/tests/account.test.js
@@ -1,86 +1,40 @@
 const Account = require('../src/account')
 
 describe('account', () => {
+  it('should start with balance equal to zero', () => {
+    const acc = new Account()
+    const result = acc.balance()
+    expect(result).toEqual(0)
+  })
   it('makes a deposit, expects the correct balance', () => {
     const acc = new Account()
-    acc.deposit(1000)
+    acc.newTransaction('deposit', 1000)
     const result = acc.balance()
     expect(result).toEqual(1000)
   })
 
   it('makes a deposit and a withdrawal, expects the correct balance', () => {
     const acc = new Account()
-    acc.deposit(1000)
+    acc.newTransaction('deposit', 1000)
     acc.balance()
-    acc.withdrawal(500)
+    acc.newTransaction('withdrawal', -500)
     const result = acc.balance()
     expect(result).toEqual(500)
   })
 
-  it('accepts a float when passed to deposit, expects number rounded with 2 decimals', () => {
+  it('accepts a float when passed to deposit, expects number with 2 decimals', () => {
     const acc = new Account()
-    acc.deposit(84.67)
+    acc.newTransaction('deposit', 84.67)
     const result = acc.balance()
     expect(result).toEqual(84.67)
   })
 
-  it('accepts a float when passed to withdrawal, expects number rounded with 2 decimals', () => {
+  it('accepts a float when passed to withdrawal, expects number with 2 decimals', () => {
     const acc = new Account()
-    acc.deposit(1000)
+    acc.newTransaction('deposit', 1000)
     acc.balance()
-    acc.withdrawal(500.25)
+    acc.newTransaction('withdrawal', -500.25)
     const result = acc.balance()
     expect(result).toEqual(499.75)
-  })
-
-  it('recognises invalid negative deposits', () => {
-    const acc = new Account()
-    expect(() => {
-      acc.deposit(-1000)
-    }).toThrow('Invalid transaction')
-  })
-
-  it('recognises invalid withdrawal', () => {
-    const acc = new Account()
-    acc.deposit(1000)
-    acc.balance()
-    expect(() => {
-      acc.withdrawal(5000)
-    }).toThrow('Invalid transaction')
-  })
-
-  it('recognises invalid negative withdrawal', () => {
-    const acc = new Account()
-    expect(() => {
-      acc.withdrawal(-1000)
-    }).toThrow('Invalid transaction')
-  })
-
-  it('recognises invalid amount for deposit', () => {
-    const acc = new Account()
-    expect(() => {
-      acc.deposit('1000')
-    }).toThrow('Invalid transaction')
-  })
-
-  it('recognises invalid amount for withdrawal', () => {
-    const acc = new Account()
-    expect(() => {
-      acc.withdrawal('1000')
-    }).toThrow('Invalid transaction')
-  })
-
-  it('should throw an error if deposit is called without an argument', () => {
-    const acc = new Account()
-    expect(() => {
-      acc.deposit()
-    }).toThrow('Invalid transaction')
-  })
-
-  it('should throw an error if withdrawal called without an argument', () => {
-    const acc = new Account()
-    expect(() => {
-      acc.withdrawal()
-    }).toThrow('Invalid transaction')
   })
 })
